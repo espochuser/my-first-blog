4b89a6cc4db4a283ea1f5091a66312ddb867da80
espochuser <silviosbet@gmail.com>
2024-11-23 02:09:32 -0500

    se agrego toodo lo nuevo

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def prueba(request):
-    return HttpResponse('Estamo en vivooooo')
 
 urlpatterns = [
-    path(route='', view=prueba, name='home'),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
 ]
